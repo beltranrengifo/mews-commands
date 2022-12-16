e149868bad828ee16e4e78f584799c6a093b5111
Beltrán Rengifo <merloc@gmail.com>
2022-12-16 14:11:27 +0100

    add some code comments

--- config.sh
+++ config.sh
@@ -4,10 +4,13 @@ INFO_COLOR='\033[1;35m'
 NEUTRAL_TEXT_COLOR='\033[0;37m'
 WARN_COLOR='\033[1;33m'
 
+## The available commands.
+## Remember to add new commands here before modifying the `mews` function
+# as the new command should exist in the `MEWS_AVAILABLE_COMMANDS` array
 COMMANDER="commander"
 NAVIGATOR="navigator"
 DISTRIBUTOR="distributor"
 STORYBOOK="storybook"
 PULL_JS="pull-js"
-
+# wrap commands in an array just for convenience
 MEWS_AVAILABLE_COMMANDS=($COMMANDER $NAVIGATOR $DISTRIBUTOR $STORYBOOK $PULL_JS)
